ee361a6cd3236a968a6383316afda5e393d52425
numberisnan <github@farazkaleemmalik.cyou>
2025-06-17 17:49:29 -0400

    Add signal to trigger rebuild on main site when there is an update

---
+++ runner/trigger_rebild.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [[ grep "No real updates!" /tmp/publish.log; ]] then
+    return
+fi
+
+git config user.name "GitHub Actions Bot"
+git config user.email "<>"
+git commit --allow-empty -m "Automated RSS update"
+
+git push origin feed
